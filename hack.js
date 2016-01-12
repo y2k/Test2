18f21e34f972e39af46653a19792a844d15d719c
y2k <itwisterlx@gmail.com>
2016-01-13 03:30:40 +0400

    Update hack.js

--- hack.js
+++ hack.js
@@ -14,9 +14,6 @@
 		var b = 2 * Math.PI - a;
 		var c = ((b - Server.floorsInfo[0].phase) / Server.floorsInfo[0].speed) - (Server.getTime() * 0.001);
 		var delay = c * 1000;
-		
-		// var x = Math.PI - ((Server.getTime() * 0.001 * Server.floorsInfo[0].speed + Server.floorsInfo[0].phase) % (2*Math.PI));
-		// var delay = (2 * Math.PI + x) / (0.001 * Server.floorsInfo[0].speed + Server.floorsInfo[0].phase)
 
 		console.log("HACK :: DELAY = " + delay);
 
