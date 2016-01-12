d3209353ac9d4ed767708740ef46bb5afa20f73d
y2k <itwisterlx@gmail.com>
2016-01-13 01:04:08 +0400

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,21 +1,21 @@
 (function() {
+	var screen = document.getElementById("screen");
+	var lastDelay = 0;
+	var tapGame = function(delay) {
+		lastDelay += delay;
+		setTimeout(function() {
+			var clickEvent = document.createEvent("MouseEvents");
+			clickEvent.initEvent("click", true, true);
+			screen.dispatchEvent(clickEvent);
+			
+			console.log("HACK :: tap on screen");
+		}, lastDelay);
+	}
+
 	Analytics.roundReplay = function() {
 		console.log("HACK :: Analytics.roundReplay");
-		
-		var screen = document.getElementById("screen");
-		var lastDelay = 0;
-		var tapGame = function(delay) {
-			lastDelay += delay;
-			setTimeout(function() {
-				var clickEvent = document.createEvent("MouseEvents");
-				clickEvent.initEvent("click", true, true);
-				screen.dispatchEvent(clickEvent);
-				
-				console.log("HACK :: tap on screen");
-			}, lastDelay);
-		}
 
-		tapGame(2000);
 		tapGame(500);
+		tapGame(400);
 	}
 })();
