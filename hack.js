93c9e7eb829a91ebbc26842f1ac172fb3bca0d10
y2k <itwisterlx@gmail.com>
2016-01-13 00:35:43 +0400

    Update hack.js

--- hack.js
+++ hack.js
@@ -3,7 +3,7 @@
 	var _console = window.console;
 	var console = {};
 	console.log = function(message){
-			_console.log("ROUTE: " + message);
+			// _console.log("ROUTE: " + message);
 	}
 	window.console = console;
 
